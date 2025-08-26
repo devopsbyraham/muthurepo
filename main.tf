3505a84461a2934d794b7d3ce709e889ba972014
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2025-08-26 08:37:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,19 @@ provider "aws" {
 region = "us-east-1"
 }
 
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "my-terraform-batch-001"
+
+    workspaces {
+      name = "muthurepo"
+    }
+  }
+}
+
 resource "aws_instance" "three" {
-  count         = 10
+  count         = 1
   ami           = "ami-00ca32bbc84273381"
   instance_type = "t2.micro"
 
